23e415bd908177c89a495a43f1bd6ebc1d15f4a8
Xavier Denis <xldenis@gmail.com>
2021-10-25 13:32:29 +0200

    wip

---
+++ creusot/tests/should_succeed/two_phase.rs
@@ -0,0 +1,13 @@
+// #![feature(register_tool, rustc_attrs)]
+// #![register_tool(creusot)]
+// #![feature(proc_macro_hygiene, stmt_expr_attributes)]
+// #![feature(type_ascription)]
+
+// extern crate creusot_contracts;
+
+// use creusot_contracts::std::*;
+// use creusot_contracts::*;
+
+fn test () {
+  v.push(v.len()); 
+}
